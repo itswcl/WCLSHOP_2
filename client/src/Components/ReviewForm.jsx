a516486550e4be37eebf935ada5d5726050b95b1
weicheng lee <z82111712@gmail.com>
2022-04-04 21:23:53 -0700

    remove useEffect from form

--- client/src/Components/ReviewForm.jsx
+++ client/src/Components/ReviewForm.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import FileBase64 from 'react-file-base64';
